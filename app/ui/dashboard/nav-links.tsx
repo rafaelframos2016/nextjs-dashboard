49311e0dd5efa38d81cb435a05c0b38d9e1f771e
Rafael Ramos <rafaelramos@MacBook-Air-de-Rafael.local>
2024-06-11 01:33:07 -0300

    começando criacao de pagina invoice

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -12,13 +12,13 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Casa', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Projeto',
+    name: 'Fatura',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Cliente', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
